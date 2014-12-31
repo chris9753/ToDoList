f54f009dd74678f7011ca8b76cfb3b99b6ba5d4c
chris9753 <mr.chrisdasilver@gmail.com>
2014-12-31 17:10:04 -0500

    todolist-app

--- ToDoList/FirstViewController.swift
+++ ToDoList/FirstViewController.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+ var toDoItems :[String] = []
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
 
+    
+    @IBOutlet var tasksTable: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,54 @@ class FirstViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toDoItems.count
+        
+        
+    }
+    
+  
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        var cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        
+        cell.contentView.backgroundColor = UIColor.yellowColor()
+        
+        cell.textLabel.text = toDoItems[indexPath.row]
+        return cell
+    }
+    override func viewWillAppear(animated: Bool) {
+        
+        //If the Permanent Object has Data clear the curent array
+      
+        if var storedToDoItems: AnyObject = NSUserDefaults.standardUserDefaults().objectForKey("toDoItems"){
+            toDoItems=[]
+            
+            //Adding data to current array in the same order.
+            
+            for var i = 0;i < storedToDoItems.count ;i++ {
+                
+                toDoItems.append(storedToDoItems[i] as NSString)
+            }
+            
+            }
+        }
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath){
+        
+        if(editingStyle == UITableViewCellEditingStyle.Delete){
+            
+            toDoItems.removeAtIndex(indexPath.row)
+            let fixedItems = toDoItems
+            
+            NSUserDefaults.standardUserDefaults().setObject(fixedItems, forKey: "toDoItems")
+            NSUserDefaults.standardUserDefaults().synchronize()
+            tasksTable.reloadData()
+
+        }
+        
+     }
+    }
 
 
-}
 
--- ToDoList/SecondViewController.swift
+++ ToDoList/SecondViewController.swift
@@ -8,18 +8,50 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
-
+class SecondViewController: UIViewController,UITextFieldDelegate {
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBOutlet weak var toDoItem: UITextField!
+    
+    @IBAction func addItem(sender: AnyObject) {
+        
+        toDoItems.append(toDoItem.text)
+        
+        //Persistent Storage 
+        let fixedItems = toDoItems
+        
+        //setting Added Data from toDoItems[one at a time] to a permanent object
+        
+        NSUserDefaults.standardUserDefaults().setObject(fixedItems, forKey: "toDoItems")
+        
+        
+       //Synchronizing/saving
+        NSUserDefaults.standardUserDefaults().synchronize()
+        
+        
+        self.view.endEditing(true)
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        self.view.endEditing(true)
+        
+    }
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        toDoItem.resignFirstResponder()
+        return true
+        
+    }
 
 }
 
